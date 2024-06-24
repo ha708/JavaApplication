d7451afe5645d2ea14adf4732782a34d5119f893
ha708 <160083092+ha708@users.noreply.github.com>
2024-06-24 11:10:04 +0900

    Main.java

---
+++ pair2/Main.java
@@ -0,0 +1,9 @@
+package pair2;
+
+public class Main {
+    public static void main(String[] args){
+        Pair2<String, Integer> p = new Pair2<>("test1",1000);
+
+        System.out.println(p);
+    }
+}
