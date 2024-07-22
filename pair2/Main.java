7861e4f6d35d6d7e6fef5bf1f86924457e43be79
ha708 <160083092+ha708@users.noreply.github.com>
2024-07-22 11:03:28 +0900

    Delete pair2 directory

--- pair2/Main.java
+++
@@ -1,9 +0,0 @@
-package pair2;
-
-public class Main {
-    public static void main(String[] args){
-        Pair2<String, Integer> p = new Pair2<>("test1",1000);
-
-        System.out.println(p);
-    }
-}
