27b924529dc2f5173962ae3fbc95de7bfbfce3ae
ha708 <160083092+ha708@users.noreply.github.com>
2024-07-22 11:28:49 +0900

    Add files via upload

---
+++ ジェネリクス/pair2/Pair2.java
@@ -0,0 +1,17 @@
+package pair2;
+
+public class Pair2<T,S> {
+    public T first;
+    public S second;
+
+    Pair2(T first,S second){
+        this.first =first;
+        this.second = second;
+    }
+
+    @Override
+    public String toString(){
+        return"(" + this.first +
+                "," + this.second + ")";
+    }
+}
